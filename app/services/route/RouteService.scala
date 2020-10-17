343fb54a86468ce35c81b4d921d641c2217c561d
dlpb <daniel.b0@ntlworld.com>
2020-10-17 17:03:35 +0100

    route service basic functionality migrated

--- app/services/route/RouteService.scala
+++ app/services/route/RouteService.scala
@@ -2,9 +2,22 @@ package services.route
 
 import com.typesafe.config.Config
 import javax.inject.Inject
+import models.helpers.JsonFileReader
 import models.route.Route
+import models.route.display.list.ListRoute
+import models.route.display.map.MapRoute
 
 class RouteService @Inject() ( config: Config ) {
 
-  def getAllRoutes(): List[Route] = List.empty
+
+  val routeFileReader = new JsonFileReader
+
+  val routes: Set[Route] = routeFileReader.readAndParse[Set[Route]](config.getString("data.routes.path"))
+
+  val mapRoutes: Set[MapRoute] = routes.map(r => MapRoute(r))
+
+  val listRoutes: Set[ListRoute] = routes.map(r => ListRoute(r))
+
+  def findRoute(from: String, to: String): Option[Route] = routes.find(r => r.from.id.equals(from) && r.to.id.equals(to))
+
 }
--- test/services/route/RouteServiceTest.scala
+++ test/services/route/RouteServiceTest.scala
@@ -1,6 +1,8 @@
 package services.route
 
 import com.typesafe.config.Config
+import models.route.display.list.{ListRoute, ListRoutePoint}
+import models.route.display.map.{MapRoute, MapRoutePoint}
 import org.mockito.MockitoSugar
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
@@ -8,16 +10,74 @@ import org.scalatest.matchers.should.Matchers
 class RouteServiceTest
   extends AnyFlatSpec with Matchers with MockitoSugar {
 
+  val singleRoute = "/singleRoute.json"
+
   "Route Service" should "return a list of all routes in the json file" in {
 
-    val mockConfig = mock[Config]
-    when(mockConfig.getString("data.static.root")).thenReturn("test/resources/singleRoute.json")
+    val service = getRouteServiceWith(singleRoute)
 
-    val service = new RouteService(mockConfig)
-
-    val routes = service.getAllRoutes()
+    val routes = service.routes
 
     routes.size should be(1)
   }
 
+  it should "find a route by from and to location ids" in {
+    val service = getRouteServiceWith(singleRoute)
+
+    val from = "DRN"
+    val to = "KYL"
+
+    val foundRoute = service.findRoute(from, to)
+
+    foundRoute.isDefined should be(true)
+    foundRoute.get.from.id should be("DRN")
+    foundRoute.get.to.id should be("KYL")
+  }
+
+  it should "not find a route by from and to location ids when they are provided back to front" in {
+    val service = getRouteServiceWith(singleRoute)
+
+    val to = "DRN"
+    val from = "KYL"
+
+    val foundRoute = service.findRoute(from, to)
+
+    foundRoute.isDefined should be(false)
+  }
+
+  it should "not find a route by from and to location ids when they dont exist" in {
+    val service = getRouteServiceWith(singleRoute)
+
+    val to = "INV"
+    val from = "WCK"
+
+    val foundRoute = service.findRoute(from, to)
+
+    foundRoute.isDefined should be(false)
+  }
+
+  it should "map routes to MapRoutes" in {
+    val service = getRouteServiceWith(singleRoute)
+
+    val routes = service.mapRoutes
+
+    routes.size should be(1)
+    routes.head should be(MapRoute(MapRoutePoint(57.31997511,-5.691316014, "Duirinish", "DRN"), MapRoutePoint(57.2797712,-5.713812582, "Kyle of Lochalsh", "KYL"), "SR", "P.12", "NR"))
+  }
+
+  it should "map routes to ListRoutes" in {
+    val service = getRouteServiceWith(singleRoute)
+
+    val routes = service.listRoutes
+
+    routes.size should be(1)
+    routes.head should be(ListRoute(ListRoutePoint("DRN", "Duirinish"), ListRoutePoint("KYL","Kyle of Lochalsh"), "P.12"))
+  }
+
+  private def getRouteServiceWith(routePath: String) = {
+    val mockConfig = mock[Config]
+    when(mockConfig.getString("data.routes.path")).thenReturn(routePath)
+    val service = new RouteService(mockConfig)
+    service
+  }
 }
